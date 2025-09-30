6500f2843d74275c3823265b1d02dada36ed43f6
Artur Myszkowski <myszkowski.artur@gmail.com>
2025-09-30 19:44:58 +0200

    Add BackgroundGradient component for improved background styling in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import About from "@/components/sections/About";
 import Skills from "@/components/sections/Skills";
 import Projects from "@/components/sections/Projects";
 import Contact from "@/components/sections/Contact";
+import BackgroundGradient from "@/components/BackgroundGradient";
 
 export default function Home() {
     const scrollToSection = (sectionId: string) => {
@@ -28,7 +29,7 @@ export default function Home() {
 
     return (
         <main className="relative text-white">
-            <div className="absolute -z-20 -inset-x-[300px] -top-72 -bottom-6 md:-inset-x-32 md:-right-48 md:-inset-y-4 md:-top-24 -rotate-[5deg] bg-gradient-to-b from-black to-gray-900" />
+            <BackgroundGradient />
 
             <NavBar onNavigate={scrollToSection} />
 
