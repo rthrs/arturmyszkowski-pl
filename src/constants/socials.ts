e4dad57363ac48af5366516002c44947c27d1f31
Artur Myszkowski <myszkowski.artur@gmail.com>
2025-10-18 11:23:19 +0200

    Add new resume PDF and update link in socials: Introduce Artur Myszkowski's 2025 resume PDF and update the corresponding link in the SOCIAL_LINKS constant for accurate navigation.

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -35,7 +35,7 @@ export const SOCIAL_LINKS = {
     },
     resume: {
         id: "resume",
-        href: "/documents/Artur_Myszkowski_Resume_2025_10.pdf",
+        href: "/documents/Artur_Myszkowski_Resume_2025_10-public.pdf",
         label: "Resume",
         icon: FiFileText,
         target: "_blank",
